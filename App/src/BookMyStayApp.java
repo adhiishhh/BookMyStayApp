1bd5f38c566cddba844fb0be0caaae95a623b14d
chillsherlock <shubhampradhan806@gmail.com>
2026-03-20 10:16:37 +0530

    Adhish Added UC2 Basic Room Types & Static Availability

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,13 +1,32 @@
-/**
- * Use Case 1: Application Entry & Welcome Message
- */
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("===== UC1: Application Entry =====");
-        System.out.println("Welcome to Book My Stay App");
-        System.out.println("System initialized successfully.\n");
+        // UC1
+        System.out.println("===== UC1 =====");
+        System.out.println("Welcome to Book My Stay App\n");
 
+        // UC2
+        System.out.println("===== UC2: Room Types =====");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("\nSingle Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        System.out.println("\nDouble Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        System.out.println("\nSuite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
