04b0cb9adfb24a32894420d07468d2c99293bb04
chillsherlock <shubhampradhan806@gmail.com>
2026-03-20 09:47:30 +0530

    Adhish Added UC1 Application Entry

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== UC1: Application Entry =====");
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("System initialized successfully.\n");
+
+    }
+}
